9817fe7db7ad72f130da1cb1723a2839bda9d0c2
hswiergiel <h.swiergiel@hotmail.fr>
2022-10-07 11:37:22 +0200

    boucle sur l animation alveoles

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // mélanger la liste
-
-const alveolusArray = Array.from(document.querySelectorAll("#grid li")).map(li => li.innerHTML);
+const alveoles = document.querySelectorAll("#grid li")
+const alveolesInnerHTML = Array.from(alveoles).map(li => li.innerHTML);
 
 function animatedHive() {
-    for (i = 0; i < document.querySelectorAll('#grid li').length; i++) {
-        document.querySelectorAll('#grid li')[i].animate([
+    for (i = 0; i < alveoles.length; i++) {
+        alveoles[i].animate([
             { opacity: 0 },
             { opacity: 1 }
         ], {
@@ -15,21 +15,21 @@ function animatedHive() {
     }
 };
 
-function fisherYatesShuffle(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1)); //random index
-        [arr[i], arr[j]] = [arr[j], arr[i]]; // swap
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) { //parcours le tableau à l'envers
+        let j = Math.floor(Math.random() * (i + 1)); //création index random
+        [array[i], array[j]] = [array[j], array[i]]; // nouvel index random sur chaque élément du tableau
     };
     for (let i = 1; i <= 16; i++) {
-        document.querySelector(`#grid li:nth-child(${i})`).innerHTML = arr[i];
+        document.querySelector(`#grid li:nth-child(${i})`).innerHTML = array[i]; // remplace innerHTML des li par celui du tableau mélangé
     };
     animatedHive();
 }
 
-fisherYatesShuffle(alveolusArray);
+shuffle(alveolesInnerHTML);
 
 document.querySelector("#shuffleButton").addEventListener("click", function () {
-    fisherYatesShuffle(alveolusArray);
+    shuffle(alveolesInnerHTML);
 });
 
 // style du formulaire
@@ -41,165 +41,42 @@ document.querySelector("#submitButton").addEventListener('click', function (even
     document.querySelector(".formPresentation").reset();
 });
 
-
 //alveoles qui se retournent
 
-document.querySelector(`#grid li:nth-child(1)`).addEventListener('mouseenter', function () {
-    document.querySelector(`#grid li:nth-child(1) .hexagon img`).animate([
-        { transform: "rotateY(0deg)", opacity: "1" },
-        { transform: "rotateY(90deg)", opacity: "0" }
-    ], 700);
-    document.querySelector(`#grid li:nth-child(1) .hexagon img`).style.opacity = "0";
-    document.querySelector(`#grid li:nth-child(1) .hexagon .box`).animate([
-        { opacity: "0" },
-        { opacity: "1" }
-    ], 700);
-    document.querySelector(`#grid li:nth-child(1) .hexagon .box`).style.opacity = "1";
-});
+for (let n = 1; n <= 16; n++) {
+    document.querySelector(`#grid li:nth-child(${n})`).addEventListener('click', function () {
+        if (document.querySelector(`#grid li:nth-child(${n}) .hexagon img`).style.opacity === "" ||
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon img`).style.opacity === "1") {
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon img`).animate([
+                { opacity: "1" },
+                { opacity: "0.1" }
+            ], 700);
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon img`).style.opacity = "0.1";
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon .box`).animate([
+                { opacity: "0" },
+                { opacity: "0.9" }
+            ], 700);
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon .box`).style.opacity = "0.9";
+        } else {
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon img`).animate([
+                { opacity: "0.1" },
+                { opacity: "1" }
+            ], 700);
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon img`).style.opacity = "1";
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon img`).style.filter = "grayscale(100%)";
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon .box`).animate([
+                { opacity: "0.9" },
+                { opacity: "0" }
+            ], 700);
+            document.querySelector(`#grid li:nth-child(${n}) .hexagon .box`).style.opacity = "0";
+        }
+    });
+    document.querySelector(`#grid li:nth-child(${n})`).addEventListener('mouseenter', function () {
+        document.querySelector(`#grid li:nth-child(${n})`).style.cursor = "pointer";
+    });
+}
 
-document.querySelector(`#grid li:nth-child(1)`).addEventListener('mouseleave', function () {
-    document.querySelector(`#grid li:nth-child(1) .hexagon img`).animate([
-        { transform: "rotateY(90deg)", opacity: "0" },
-        { transform: "rotateY(0deg)", opacity: "1" }
-    ], 700);
-    document.querySelector(`#grid li:nth-child(1) .hexagon img`).style.opacity = "1";
-    document.querySelector(`#grid li:nth-child(1) .hexagon img`).style.filter = "grayscale(100%)";
-    document.querySelector(`#grid li:nth-child(1) .hexagon .box`).animate([
-        { opacity: "1" },
-        { opacity: "0" }
-    ], 700);
-    document.querySelector(`#grid li:nth-child(1) .hexagon .box`).style.opacity = "0";
-});
-
-document.querySelector(`#grid li:nth-child(2)`).addEventListener('mouseenter', function () {
-    document.querySelector(`#grid li:nth-child(2) .hexagon img`).animate([
-        { opacity: "1", filter: "blur(0)" },
-        { opacity: "0", filter: "blur(2rem)" }
-    ], 700);
-    document.querySelector(`#grid li:nth-child(2) .hexagon img`).style.opacity = "0";
-    document.querySelector(`#grid li:nth-child(2) .hexagon .box`).animate([
-        { opacity: "0" },
-        { opacity: "1" }
-    ], 700);
-    document.querySelector(`#grid li:nth-child(2) .hexagon .box`).style.opacity = "1";
-});
-
-document.querySelector(`#grid li:nth-child(2)`).addEventListener('mouseleave', function () {
-    document.querySelector(`#grid li:nth-child(2) .hexagon img`).animate([
-        { opacity: "0", filter: "blur(2rem)" },
-        { opacity: "1", filter: "blur(0)" }
-    ], 700);
-    document.querySelector(`#grid li:nth-child(2) .hexagon img`).style.opacity = "1";
-    document.querySelector(`#grid li:nth-child(2) .hexagon img`).style.filter = "grayscale(100%)";
-    document.querySelector(`#grid li:nth-child(2) .hexagon .box`).animate([
-        { opacity: "1" },
-        { opacity: "0" }
-    ], 700);
-    document.querySelector(`#grid li:nth-child(2) .hexagon .box`).style.opacity = "0";
-});
-
-document.querySelector(`#grid li:nth-child(3)`).addEventListener('click', function () {
-    if (document.querySelector(`#grid li:nth-child(3) .hexagon img`).style.opacity === "" ||
-        document.querySelector(`#grid li:nth-child(3) .hexagon img`).style.opacity === "1") {
-        document.querySelector(`#grid li:nth-child(3) .hexagon img`).animate([
-            { opacity: "1" },
-            { opacity: "0.1" }
-        ], 700);
-        document.querySelector(`#grid li:nth-child(3) .hexagon img`).style.opacity = "0.1";
-        document.querySelector(`#grid li:nth-child(3) .hexagon .box`).animate([
-            { opacity: "0" },
-            { opacity: "0.9" }
-        ], 700);
-        document.querySelector(`#grid li:nth-child(3) .hexagon .box`).style.opacity = "0.9";
-    } else {
-        document.querySelector(`#grid li:nth-child(3) .hexagon img`).animate([
-            { opacity: "0.1" },
-            { opacity: "1" }
-        ], 700);
-        document.querySelector(`#grid li:nth-child(3) .hexagon img`).style.opacity = "1";
-        document.querySelector(`#grid li:nth-child(3) .hexagon img`).style.filter = "grayscale(100%)";
-        document.querySelector(`#grid li:nth-child(3) .hexagon .box`).animate([
-            { opacity: "0.9" },
-            { opacity: "0" }
-        ], 700);
-        document.querySelector(`#grid li:nth-child(3) .hexagon .box`).style.opacity = "0";
-    }
-});
-document.querySelector(`#grid li:nth-child(3)`).addEventListener('mouseenter', function () {
-    document.querySelector(`#grid li:nth-child(3)`).style.cursor = "pointer";
-});
-
-//boucle essai 1
-// for (i = 1; i <= 16; i++) {
-//     document.querySelector(`#grid li:nth-child(${i})`).addEventListener('mouseenter', function () {
-//         document.querySelector(`#grid li:nth-child(${i}) .hexagon img`).animate([
-//             { transform: "rotateY(0deg)", opacity: "1" },
-//             { transform: "rotateY(90deg)", opacity: "0" }
-//             // { opacity: "1", filter: "blur(0)" },
-//             // { opacity: "0", filter: "blur(2rem)" }
-//         ], 700);
-//         document.querySelector(`#grid li:nth-child(${i}) .hexagon img`).style.opacity = "0";
-//         document.querySelector(`#grid li:nth-child(${i}) .hexagon .box`).animate([
-//             { opacity: "0" },
-//             { opacity: "1" }
-//         ], 700);
-//         document.querySelector(`#grid li:nth-child(${i}) .hexagon .box`).style.opacity = "1";
-//     });
-
-//     document.querySelector(`#grid li:nth-child(${i})`).addEventListener('mouseleave', function () {
-//         document.querySelector(`#grid li:nth-child(${i}) .hexagon img`).animate([
-//             { transform: "rotateY(90deg)", opacity: "0" },
-//             { transform: "rotateY(0deg)", opacity: "1" }
-//             // { opacity: "0", filter: "blur(2rem)" },
-//             // { opacity: "1", filter: "blur(0)" }
-//         ], 700);
-//         document.querySelector(`#grid li:nth-child(${i}) .hexagon img`).style.opacity = "1";
-//         document.querySelector(`#grid li:nth-child(${i}) .hexagon img`).style.filter = "grayscale(100%)";
-//         document.querySelector(`#grid li:nth-child(${i}) .hexagon .box`).animate([
-//             { opacity: "1" },
-//             { opacity: "0" }
-//         ], 700);
-//         document.querySelector(`#grid li:nth-child(${i}) .hexagon .box`).style.opacity = "0";
-//     });
-// }
-
-//boucle essai 2
-// let AllLi = document.querySelectorAll("#grid li");
-// let Allimg = document.querySelectorAll("#grid li hexagon > img");
-// let Allboxes = document.querySelectorAll("#grid li .hexagon .box");
-
-// for (i = 1; i <= 16; i++) {
-//     AllLi[i].addEventListener('mouseenter', function () {
-//         Allimg[i].animate([
-//             { transform: "rotateY(0deg)", opacity: "1" },
-//             { transform: "rotateY(90deg)", opacity: "0" }
-//             // { opacity: "1", filter: "blur(0)" },
-//             // { opacity: "0", filter: "blur(2rem)" }
-//         ], 700);
-//         Allimg[i].style.opacity = "0";
-//         Allboxes[i].animate([
-//             { opacity: "0" },
-//             { opacity: "1" }
-//         ], 700);
-//         Allboxes[i].style.opacity = "1";
-//     });
-
-//     AllLi[i].addEventListener('mouseleave', function () {
-//         Allimg[i].animate([
-//             { transform: "rotateY(90deg)", opacity: "0" },
-//             { transform: "rotateY(0deg)", opacity: "1" }
-//             // { opacity: "0", filter: "blur(2rem)" },
-//             // { opacity: "1", filter: "blur(0)" }
-//         ], 700);
-//         Allimg[i].style.opacity = "1";
-//         Allimg[i].style.filter = "grayscale(100%)";
-//         Allboxes[i].animate([
-//             { opacity: "1" },
-//             { opacity: "0" }
-//         ], 700);
-//         Allboxes[i].style.opacity = "0";
-//     });
-// }
+// menu burger
 
 const closeButton = document.querySelector('.closemenu');
 const openButton = document.querySelector('.openmenu');
